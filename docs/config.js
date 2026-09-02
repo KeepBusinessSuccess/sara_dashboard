54a423039875e51106759f4e89993c71356f607b
KeepBusinessSuccess <innovacion@kbsbusiness.com>
2026-09-02 13:35:48 -0600

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,6 @@
 window.SARA_GITHUB_CONFIG = Object.freeze({
   googleClientId: '55530186275-cig6tqb3dnmjjnt5adrv0fpqqp2h2mq3.apps.googleusercontent.com',
-  gatewayDeploymentId: 'AKfycbztH5HMSnYl7Ak0agBzaK0ojS3T4p1YXzLZKPL39aOjRcJGmtRq5iI2YpkNwTkt0IAicA',
+  gatewayDeploymentId: 'AKfycbxjLWvkRctgXGDFVwB2_FX5_ny0uhlGUYoRKb8XbdUMLzu-IvPe3SGXJv6l02IhTpfv',
   allowedDomain: 'kbsbusiness.com',
   superAdminEmail: 'innovacion@kbsbusiness.com',
   demoMode: false,
