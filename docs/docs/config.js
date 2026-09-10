5a2cdafff1ae465eeb0a57ad8322b8bf7a11ab7f
KeepBusinessSuccess <innovacion@kbsbusiness.com>
2026-09-10 13:42:10 -0600

    Add files via upload

---
+++ docs/docs/config.js
@@ -0,0 +1,11 @@
+window.SARA_GITHUB_CONFIG = Object.freeze({
+  googleClientId: '55530186275-cig6tqb3dnmjjnt5adrv0fpqqp2h2mq3.apps.googleusercontent.com',
+  gatewayDeploymentId: 'AKfycbxjLWvkRctgXGDFVwB2_FX5_ny0uhlGUYoRKb8XbdUMLzu-IvPe3SGXJv6l02IhTpfv',
+  allowedDomain: 'kbsbusiness.com',
+  superAdminEmail: 'innovacion@kbsbusiness.com',
+  demoMode: false,
+  oauthScopes: [
+    'https://www.googleapis.com/auth/script.external_request',
+    'https://www.googleapis.com/auth/userinfo.email'
+  ]
+});
